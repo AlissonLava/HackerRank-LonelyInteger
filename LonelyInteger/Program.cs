70536cd351f7bb5c6fc3cff847fcd0eba17ab602
AlissonLava <61854869+AlissonLava@users.noreply.github.com>
2022-12-23 14:55:55 -0300

    Add project files.

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        int numUnique = a[0];
+
+        if (a.Count > 1)
+        {
+            for (int i = 0; i < (a.Count); i++)
+            {
+
+                bool unique = true;
+                for (int j = 0; j < (a.Count); j++)
+                {
+                    if ((i != j) && (a[i] == a[j]))
+                    {
+                        unique = false;
+                    }
+                }
+
+                if (unique)
+                {
+                    numUnique = a[i];
+                }
+            }
+        }
+
+        return numUnique;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        Console.WriteLine(result);
+    }
+}
